7fc9b13421e9df6329cd2bb61cf05484ee8fbbe5
jurifitz <juri.fitz@students.unibe.ch>
2021-03-17 14:28:45 +0100

    function

--- exercise3.py
+++ exercise3.py
@@ -1,4 +1,12 @@
+##"C:/Users/jfafi/PycharmProjects/python_ex3/precip_data.txt"
+
 import numpy
 
-with open ("filename","mode") as f:
-    read_data = f.read()
+def filereader(path):
+    precip = []
+    f = open(path, "r")
+    for line in f:
+        precip.append(line[26:30])
+    return precip
+
+my = numpy.array(xi[1:]).astype(numpy.float)
