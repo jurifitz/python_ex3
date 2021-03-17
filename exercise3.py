760095b23ef93e5a26fb87f35c571f5c1970aa3c
jurifitz <juri.fitz@students.unibe.ch>
2021-03-17 15:04:25 +0100

    corrected

--- exercise3.py
+++ exercise3.py
@@ -1,8 +1,8 @@
 import numpy
 import filereader
 
-precipi = filereader.file() ##insert file path
-mydata = numpy.array(precipi[1:]).astype(numpy.float)
+precipi = filereader.file("C:/Users/jfafi/Desktop/Uni/Master/1.Semester/Seminar in Geodatenanalyse und Modellierung/03/precip_data.txt") ##insert file path
+mydata = numpy.array(precipi[1:]).astype(float)
 print(f"Minimal precipitation: {mydata.min()}")
 print(f"Maximal precipitation: {mydata.max()}")
 print(f"Mean precipitation: {mydata.mean()}")
